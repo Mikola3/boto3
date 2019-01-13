2637cfa6f90a60e30cfd375d64e099830c204c27
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2019-01-13 16:39:54 +0300

    Add files via upload

---
+++ get-private-ip-ec2.py
@@ -0,0 +1,12 @@
+def get_addr(stack_name):
+    autoscaling_group_id = cloudformation_client.describe_stack_resource(
+        StackName=args.stack,
+        LogicalResourceId="SolrAutoScalingGroup"
+    )['StackResourceDetail']['PhysicalResourceId']
+
+    # Find ids of instances in autoscaling groups
+    instance_id = autoscaling_client.describe_auto_scaling_groups(
+        AutoScalingGroupNames=[autoscaling_group_id]
+    )['AutoScalingGroups'][0]['Instances'][0]['InstanceId']
+
+    return boto3.resource('ec2').Instance(instance_id).private_ip_address
